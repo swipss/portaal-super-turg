bf4978d0141f693f1231e68bdeffd94ae7908b09
swipss <vassiljevsten1@gmail.com>
2022-09-19 20:56:41 +0300

    progress bar on page loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,19 +4,32 @@ import Router from 'next/router';
 import '../public/globals.css';
 import { useState } from 'react';
 import { Loader } from '../components/Loader';
+import Head from 'next/head';
+import NProgress from 'nprogress';
 
 const App = ({ Component, pageProps }: AppProps) => {
   const [loading, setLoading] = useState(false);
   Router.events.on('routeChangeStart', (url) => {
+    NProgress.start();
     setLoading(true);
   });
 
   Router.events.on('routeChangeComplete', (url) => {
+    NProgress.done();
     setLoading(false);
   });
 
   return (
     <SessionProvider session={pageProps.session}>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+          integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </Head>
       {loading && <Loader />}
       <Component {...pageProps} />
     </SessionProvider>
