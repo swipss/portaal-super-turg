00b3925edf0b2deaf188d58c7b7d301bbc10acc5
swipss <vassiljevsten1@gmail.com>
2022-12-06 22:50:56 +0200

    refactor code and use trpc

--- components/Layouts/Layout.tsx
+++ components/Layouts/Layout.tsx
@@ -1,7 +1,4 @@
-import { GetStaticProps } from 'next';
-import Head from 'next/head';
 import React, { ReactNode } from 'react';
-import prisma from '../../lib/prisma';
 import Header from './Header/Header';
 
 type Props = {
@@ -10,15 +7,13 @@ type Props = {
 
 const Layout: React.FC<Props> = ({
   children,
-  userPosts,
 }: {
   children: React.ReactNode;
   userPosts: any;
 }) => (
   <div className="bg-gray-100">
-    {userPosts}
     <Header />
-    <div className="p-5 max-w-5xl mx-auto">{children}</div>
+    <div className="max-w-5xl p-5 mx-auto">{children}</div>
   </div>
 );
 
